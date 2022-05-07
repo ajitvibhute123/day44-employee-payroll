154cf4ac9e3ed8cb7cc233375f790bf9d32a51c6
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-05-07 22:16:58 +0530

    Added : Ability to save the employee payroll object to local storage.

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -34,3 +34,46 @@ const save = () => {
         return;
     }
 }
+//UC4
+const createAndUpdateStorage = function (employeePayrollData) {
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if (employeePayrollList != undefined) {
+        employeePayrollList = employeePayrollList.push(employeePayrollData)
+    } else {
+        employeePayrollList = [employeePayrollData] //need to ask in doubt session
+    }
+    alert(employeePayrollList.toString())
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList))
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData(); //creation of object
+    try {
+        employeePayrollData.name = getInputValueById('#name');
+    } catch (e) {
+        setTextValue('.text-error', e)
+        throw e;
+    }
+
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop()
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop()
+    employeePayrollData.department = getSelectedValues('[name=department]')
+    employeePayrollData.salary = getInputValueById('#salary')
+    employeePayrollData.note = getInputValueById('#notes')
+    let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " + getInputValueById('#year');
+    employeePayrollData.date = Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach((item) => {
+        if (item.checked) {
+            selItems.push(item.value)
+        }
+    })
+    return selItems;
+}
